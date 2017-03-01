687708d06cfd5b375573a34d3ce14d0375c9a67b
Michael L Parks <github@wmi-inc.com>
2017-03-01 09:46:49 -0500

    Added Button Configuration File

---
+++ js/modules/Button.js
@@ -0,0 +1,6 @@
+function Button(config){
+  this.name = config.name;
+  this.type = config.type;
+  this.color = config.color;
+  this.action = config.action;
+}
---
+++ js/modules/buttonConfig.js
@@ -0,0 +1,173 @@
+var buttonConfig = [
+  {
+    "key":1,
+    "name":"clear",
+    "label":"clear",
+    "type":"square",
+    "color":"gray",
+    "value":"AC",
+    "action":function(){console.log('This is the CLEAR button...');}
+  },
+  {
+    "key":2,
+    "name":"opposite",
+    "label":"opposite",
+    "type":"square",
+    "color":"gray",
+    "value":"+/-",
+    "action":function(){console.log('This is the OPPOSITE button...');}
+  },
+  {
+    "key":3,
+    "name":"percent",
+    "label":"percent",
+    "type":"square",
+    "color":"gray",
+    "value":"%",
+    "action":function(){console.log('This is the PERCENT button...');}
+  },
+  {
+    "key":4,
+    "name":"divide",
+    "label":"divide",
+    "type":"square",
+    "color":"orange",
+    "value":"÷",
+    "action":function(){console.log('This is the DIVIDE button...');}
+  },
+  {
+    "key":5,
+    "name":"seven",
+    "label":"seven",
+    "type":"square",
+    "color":"white",
+    "value":"7",
+    "action":function(){console.log('This is the SEVEN button...');}
+  },
+  {
+    "key":6,
+    "name":"eight",
+    "label":"eight",
+    "type":"square",
+    "color":"white",
+    "value":"8",
+    "action":function(){console.log('This is the EIGHT button...');}
+  },
+  {
+    "key":7,
+    "name":"nine",
+    "label":"nine",
+    "type":"square",
+    "color":"white",
+    "value":"9",
+    "action":function(){console.log('This is the NINE button...');}
+  },
+  {
+    "key":8,
+    "name":"multiply",
+    "label":"multiply",
+    "type":"square",
+    "color":"orange",
+    "value":"x",
+    "action":function(){console.log('This is the MULTIPLY button...');}
+  },
+  {
+    "key":9,
+    "name":"four",
+    "label":"four",
+    "type":"square",
+    "color":"white",
+    "value":"4",
+    "action":function(){console.log('This is the FOUR button...');}
+  },
+  {
+    "key":10,
+    "name":"five",
+    "label":"five",
+    "type":"square",
+    "color":"white",
+    "value":"5",
+    "action":function(){console.log('This is the FIVE button...');}
+  },
+  {
+    "key":11,
+    "name":"six",
+    "label":"six",
+    "type":"square",
+    "color":"white",
+    "value":"6",
+    "action":function(){console.log('This is the SIX button...');}
+  },
+  {
+    "key":12,
+    "name":"subtract",
+    "label":"subtract",
+    "type":"square",
+    "color":"orange",
+    "value":"-",
+    "action":function(){console.log('This is the SUBTRACT button...');}
+  },
+  {
+    "key":13,
+    "name":"one",
+    "label":"one",
+    "type":"square",
+    "color":"white",
+    "value":"1",
+    "action":function(){console.log('This is the ONE button...');}
+  },
+  {
+    "key":14,
+    "name":"two",
+    "label":"two",
+    "type":"square",
+    "color":"white",
+    "value":"2",
+    "action":function(){console.log('This is the TWO button...');}
+  },
+  {
+    "key":15,
+    "name":"three",
+    "label":"three",
+    "type":"square",
+    "color":"white",
+    "value":"3",
+    "action":function(){console.log('This is the THREE button...');}
+  },
+  {
+    "key":16,
+    "name":"add",
+    "label":"add",
+    "type":"square",
+    "color":"orange",
+    "value":"+",
+    "action":function(){console.log('This is the ADD button...');}
+  },
+  {
+    "key":17,
+    "name":"zero",
+    "label":"zero",
+    "type":"rectangle",
+    "color":"white",
+    "value":"0",
+    "action":function(){console.log('This is the ZERO button...');}
+  },
+  {
+    "key":18,
+    "name":"decimal",
+    "label":"decimal",
+    "type":"square",
+    "color":"white",
+    "value":".",
+    "action":function(){console.log('This is the DECIMAL button...');}
+  },
+  {
+    "key":19,
+    "name":"equal",
+    "label":"equal",
+    "type":"square",
+    "color":"orange",
+    "value":"=",
+    "action":function(){console.log('This is the EQUAL button...');}
+  }
+];
